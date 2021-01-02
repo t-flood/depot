fed1180937cc8447331cb24044ef6fc49c67c1cd
Tim Flood <tflood9@gmail.com>
2021-01-02 15:53:27 -0500

    Added validation for minimum 10 character title

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness:true
   validates :image_url, allow_blank:true, format: {with:%r{\.(gif|jpg|png)\Z}i, message:'must be a URL for GIF, JPG, or PNG image.'}
+  validates :title, length: {minimum:10}
 end
