45726356267c1abbadcae0036155922947212e66
Tim Flood <tflood9@gmail.com>
2021-01-31 20:52:33 -0500

    added tests for JS

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -10,32 +10,32 @@ class OrdersTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Orders"
   end
 
-  test "creating a Order" do
+  test "creating an Order" do
     visit orders_url
     click_on "New Order"
 
-    fill_in " pay type", with: @order. pay_type
+    fill_in "Pay type", with: @order.pay_type
     fill_in "Address", with: @order.address
     fill_in "Email", with: @order.email
     fill_in "Name", with: @order.name
     click_on "Create Order"
 
     assert_text "Order was successfully created"
-    click_on "Back"
+    #click_on "Back"
   end
 
   test "updating a Order" do
     visit orders_url
     click_on "Edit", match: :first
 
-    fill_in " pay type", with: @order. pay_type
+    fill_in "Pay type", with: @order.pay_type
     fill_in "Address", with: @order.address
     fill_in "Email", with: @order.email
     fill_in "Name", with: @order.name
     click_on "Update Order"
 
     assert_text "Order was successfully updated"
-    click_on "Back"
+    #click_on "Back"
   end
 
   test "destroying a Order" do
@@ -45,5 +45,22 @@ class OrdersTest < ApplicationSystemTestCase
     end
 
     assert_text "Order was successfully destroyed"
+  end
+
+  test "checking routing number" do
+    visit store_index_url
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    assert_no_selector
+    "#order_routing_number"
+
+    select 'Check',from: 'Pay type'
+
+    assert_selector
+    "#order_routing_number"
   end
 end
